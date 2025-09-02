c1771b58dc4bcb833469615174a7600fe8c36975
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-09-03 00:12:14 +0530

    improve login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,27 @@
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { GithubIcon } from 'lucide-react'
+
 export default function LoginPage() {
   return (
-    <div>
-      <h1>Hello Login page</h1>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle className='text-xl'>Welcome back our lms!</CardTitle>
+        <CardDescription>Loging with GitHub or Email Account</CardDescription>
+      </CardHeader>
+
+      <CardContent>
+        <Button className='w-full' variant={'outline'}>
+          <GithubIcon className='size-4' />
+          Sign in with GitHun
+        </Button>
+      </CardContent>
+    </Card>
   )
 }

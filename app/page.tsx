866cbe58604dd27fcdbfb58a07d46cc29b724af7
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-09-03 00:25:03 +0530

    Adding dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { ThemeToggle } from '@/components/ui/themeToggle'
 import Image from 'next/image'
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
       <h1 className='text-3xl font-bold underline text-red-500'>
         Hello, Buss LMS!
       </h1>
+      <ThemeToggle />
     </div>
   )
 }

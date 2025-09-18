57a4decef5a54e2a0c858409718c41ddc3c1773a
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-09-18 17:38:58 +0530

    Completed github auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,39 @@
+'use client'
+
 import { ThemeToggle } from '@/components/ui/themeToggle'
-
-import { headers } from 'next/headers'
-import { auth } from '@/lib/auth'
 import { Button } from '@/components/ui/button'
+import { authClient } from '@/lib/auth-client'
+import { useRouter } from 'next/navigation'
+import { toast } from 'sonner'
 
-export default async function Home() {
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  })
+export default function Home() {
+  const router = useRouter()
+  const { data: session } = authClient.useSession()
+  async function signOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push('/')
+          toast.success('Signed out successfully')
+        },
+      },
+    })
+  }
   return (
     <div className='p-24'>
       <h1 className='text-3xl font-bold underline text-red-500'>
         Hello, Buss LMS!
       </h1>
       <ThemeToggle />
-      {session ? <p>{session.user?.name}</p> : <Button>Logout</Button>}
+      {session ? (
+        <div>
+          {' '}
+          <p>{session.user?.name}</p>
+          <Button onClick={signOut}>Logout</Button>
+        </div>
+      ) : (
+        <Button>Login</Button>
+      )}
     </div>
   )
 }

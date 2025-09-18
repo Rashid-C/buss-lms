f5855b98ce8ebe7358739395e1c8bb10b212e5b5
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-09-18 16:38:18 +0530

    Better auth with github signin success

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { Button } from '@/components/ui/button'
 import {
   Card,
@@ -8,9 +9,29 @@ import {
 } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import { GithubIcon } from 'lucide-react'
+import { authClient } from '@/lib/auth-client'
+import { GithubIcon, Loader } from 'lucide-react'
+import { useTransition } from 'react'
+import { toast } from 'sonner'
 
 export default function LoginPage() {
+  const [githubPending, startGithubTransition] = useTransition()
+  async function signInWithGithub() {
+    startGithubTransition(async () => {
+      await authClient.signIn.social({
+        provider: 'github',
+        callbackURL: '/',
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success('Singed in with GitHub, you will be redirected...')
+          },
+          onError: (error) => {
+            toast.error("Internal Server Error, can't sign in with GitHub")
+          },
+        },
+      })
+    })
+  }
   return (
     <Card>
       <CardHeader>
@@ -19,9 +40,23 @@ export default function LoginPage() {
       </CardHeader>
 
       <CardContent className='flex flex-col gap-4'>
-        <Button className='w-full' variant={'outline'}>
-          <GithubIcon className='size-4' />
-          Sign in with GitHub
+        <Button
+          disabled={githubPending}
+          onClick={signInWithGithub}
+          className='w-full'
+          variant={'outline'}
+        >
+          {githubPending ? (
+            <>
+              <Loader className='size-4 animate-spin' />
+              <span>Loading...</span>
+            </>
+          ) : (
+            <>
+              <GithubIcon className='size-4' />
+              Sign in with GitHub
+            </>
+          )}
         </Button>
         <div className='relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border'>
           <span className='relative z-10 bg-card px-2 text-muted-foreground'>

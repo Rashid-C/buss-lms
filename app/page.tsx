7c7dfa09663e644ffb69d332cd15949564df6f99
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-09-18 17:15:42 +0530

    Added SSR in Login page,with private folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
 import { ThemeToggle } from '@/components/ui/themeToggle'
-import Image from 'next/image'
 
-export default function Home() {
+import { headers } from 'next/headers'
+import { auth } from '@/lib/auth'
+import { Button } from '@/components/ui/button'
+
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
   return (
-    <div>
+    <div className='p-24'>
       <h1 className='text-3xl font-bold underline text-red-500'>
         Hello, Buss LMS!
       </h1>
       <ThemeToggle />
+      {session ? <p>{session.user?.name}</p> : <Button>Logout</Button>}
     </div>
   )
 }

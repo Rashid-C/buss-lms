b11544948d1445047492259c0c0b80b489729fd1
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-09-19 23:52:48 +0530

    set up email verification otp

--- app/(auth)/login/_components/LoginForm.tsx
+++ app/(auth)/login/_components/LoginForm.tsx
@@ -10,11 +10,15 @@ import {
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { authClient } from '@/lib/auth-client'
-import { GithubIcon, Loader } from 'lucide-react'
-import { useTransition } from 'react'
+import { GithubIcon, Loader, Loader2, Send } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { useState, useTransition } from 'react'
 import { toast } from 'sonner'
 export function LoginForm() {
+  const router = useRouter()
   const [githubPending, startGithubTransition] = useTransition()
+  const [emailPending, startEmailTransition] = useTransition()
+  const [email, setEmail] = useState('')
   async function signInWithGithub() {
     startGithubTransition(async () => {
       await authClient.signIn.social({
@@ -26,6 +30,24 @@ export function LoginForm() {
           },
           onError: (error) => {
             toast.error("Internal Server Error, can't sign in with GitHub")
+          },
+        },
+      })
+    })
+  }
+
+  function signInWithEmail() {
+    startEmailTransition(async () => {
+      await authClient.emailOtp.sendVerificationOtp({
+        email: email,
+        type: 'sign-in',
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success('Email send')
+            router.push(`/verify-request`)
+          },
+          onError: () => {
+            toast.error('Error sending email')
           },
         },
       })
@@ -66,10 +88,28 @@ export function LoginForm() {
         <div className='grid gap-3'>
           <div className='grid gap-2'>
             <Label htmlFor='email'>Email</Label>
-            <Input type='email' placeholder='a@email.com' />
+            <Input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type='email'
+              placeholder='a@email.com'
+              required
+            />
           </div>
 
-          <Button>Continue with Email</Button>
+          <Button onClick={signInWithEmail} disabled={emailPending}>
+            {emailPending ? (
+              <>
+                <Loader2 className='size-4 animate-spin' />
+                <span>Loading...</span>
+              </>
+            ) : (
+              <>
+                <Send className='size-4' />
+                <span>Continue with Email</span>
+              </>
+            )}
+          </Button>
         </div>
       </CardContent>
     </Card>

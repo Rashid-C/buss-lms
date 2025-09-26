a7926479d18a0915ca689266c40a31ad3e4c79f3
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-09-26 17:08:53 +0530

    updatd signout functionality

--- app/(public)/_components/UserDropdown.tsx
+++ app/(public)/_components/UserDropdown.tsx
@@ -18,9 +18,7 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 import Link from 'next/link'
-import { authClient } from '@/lib/auth-client'
-import { useRouter } from 'next/navigation'
-import { toast } from 'sonner'
+import { useSignOut } from '@/hooks/use-signout'
 
 interface iAppProps {
   name: string
@@ -29,21 +27,7 @@ interface iAppProps {
 }
 
 export function UserDropdown({ name, email, image }: iAppProps) {
-  const router = useRouter()
-
-  async function signOut() {
-    await authClient.signOut({
-      fetchOptions: {
-        onSuccess: () => {
-          router.push('/')
-          toast.success('Signed out successfully')
-        },
-        onError: () => {
-          toast.error('failed to sign out')
-        },
-      },
-    })
-  }
+  const handleSignOut = useSignOut()
 
   return (
     <DropdownMenu>
@@ -96,7 +80,7 @@ export function UserDropdown({ name, email, image }: iAppProps) {
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
 
-        <DropdownMenuItem onClick={signOut}>
+        <DropdownMenuItem onClick={handleSignOut}>
           <LogOutIcon size={16} className='opacity-60' aria-hidden='true' />
           <span>Logout</span>
         </DropdownMenuItem>

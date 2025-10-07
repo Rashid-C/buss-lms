7f482f4b700bd5ac3c352a79e9d682fd07316768
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-10-07 23:19:43 +0530

    updated header and side bar of admin  dashboard

--- components/sidebar/app-sidebar.tsx
+++ components/sidebar/app-sidebar.tsx
@@ -16,9 +16,9 @@ import {
 } from '@tabler/icons-react'
 import Logo from '@/public/logo.svg'
 
-import { NavMain } from '@/components/nav-main'
-import { NavSecondary } from '@/components/nav-secondary'
-import { NavUser } from '@/components/nav-user'
+import { NavMain } from '@/components/sidebar/nav-main'
+import { NavSecondary } from '@/components/sidebar/nav-secondary'
+import { NavUser } from '@/components/sidebar/nav-user'
 import {
   Sidebar,
   SidebarContent,
